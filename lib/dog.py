3a556898de49bd83b56c1ecd8e0a9a75f4c9fa7e
zeitunmohamed <zeitunmohamed@gmail.com>
2025-06-15 22:05:10 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
-
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
-
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
